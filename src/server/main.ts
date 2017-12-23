8d095e8eca2f0ff158fa14b5e9922b032ef5f38d
Kenichi Saito <hajime@studiohff.net>
2017-12-23 20:23:43 +0900

    サーバにクライアントトップページ用の静的ファイルをサーブさせる

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,3 +1,13 @@
-const message: string = "hello world";
+#!/usr/bin/env node
 
-console.log(message);
+import * as path from "path";
+
+import * as Koa from "koa";
+import * as KoaStatic from "koa-static";
+
+const app = new Koa();
+
+app.use(KoaStatic(path.join(__dirname, "../../static")));
+app.use(KoaStatic(path.join(__dirname, "../client")));
+
+app.listen(3000);
