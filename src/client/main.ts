9494696b2a28913b3e197f1d4cc24b1d7b76d65f
Kenichi Saito <hajime@studiohff.net>
2017-12-23 21:11:26 +0900

    Flexboxの要素をVue.jsにレンダリングさせる

--- src/client/main.ts
+++ src/client/main.ts
@@ -4,7 +4,11 @@ import Vue from "vue";
 const app = new Vue({
     el: "#app",
     data: {
-        message: "にゃーんにゃーん"
+        items: [
+             1,  2,  3,  4,  5,  6,  7,  8,  9, 10,
+            11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+            21, 22, 23, 24, 25
+        ]
     }
 });
 /* tslint:enable:object-literal-sort-keys */
