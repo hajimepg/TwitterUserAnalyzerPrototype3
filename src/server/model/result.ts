f8a0fa740c0a49f3d61c8e53c8431d1a76fafc12
Kenichi Saito <hajime@studiohff.net>
2017-12-28 21:51:49 +0900

    未使用ファイルを削除

--- src/server/model/result.ts
+++
@@ -1,9 +0,0 @@
-import User from "./user";
-
-export default class Result {
-    public followers: User[];
-    public friends: User[];
-    public followEachOther: User[];
-    public followedOnly: User[];
-    public followOnly: User[];
-}
