aa2b2f2458aae7524f9a656d48cbe1300cacd68c
Kenichi Saito <hajime@studiohff.net>
2017-12-28 21:25:14 +0900

    ダウンロード結果通知を真っ当に

--- src/server/profileImageDownloader.ts
+++ src/server/profileImageDownloader.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 import * as lodash from "lodash";
 
 import User from "./model/user";
+import ProfileImageDownloadResult from "./profileImageDownloadResult";
 import ProfileImageRepository from "./profileImageRepository";
 
 export default class ProfileImageDownloader {
@@ -14,38 +15,41 @@ export default class ProfileImageDownloader {
     }
 
     public download() {
-        return new Promise<void>(async (resolve, reject) => {
+        return new Promise<ProfileImageDownloadResult>(async (resolve, reject) => {
+            const result = new ProfileImageDownloadResult();
+
             while (true) {
                 const target = this.downloadQueue.shift();
 
                 if (target === undefined) {
-                    resolve();
+                    resolve(result);
                     return;
                 }
 
                 const oldProfileImage = await ProfileImageRepository.find(target.screenName);
                 if (oldProfileImage !== null && oldProfileImage.sourceUrl === target.profileImageUrl) {
-                    console.log(`profile image for ${target.screenName} is already downloaded. skip.`);
+                    result.skip.push(target);
                     continue;
                 }
 
                 const response = await axios.get(target.profileImageUrl, { responseType: "arraybuffer" });
 
                 if (response.status !== 200) {
-                    console.log(`download ${target.profileImageUrl} failed. statusCode=${response.status}`);
+                    result.fail.push({ user: target, reason: `statusCode=${response.status}` });
                     continue;
                 }
 
                 const contentType = response.headers["content-type"];
                 const extension = this.getExtension(contentType);
                 if (extension === "") {
-                    console.log(`Unsupported content-type: ${contentType}`);
+                    result.fail.push({ user: target, reason: `Unsupported content-type: ${contentType}` });
                     continue;
                 }
 
                 const filename = `${target.screenName}.${extension}`;
                 ProfileImageRepository.upsert(target.screenName, target.profileImageUrl, filename,
                     new Buffer(response.data, "binary"));
+                result.success.push(target);
             }
         });
     }
